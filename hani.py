962302a15ca92b967e5ef65f385462550ec1b75c
hanialsab3 <hma115@mail.aub.edu>
2021-01-23 21:06:36 +0200

    df2

--- hani.py
+++ hani.py
@@ -1,1 +1,17 @@
-Print ("hello world")
+import numpy as np
+import pandas as pd
+
+df2 = pd.DataFrame(
+     {
+         "A": 1.0,
+         "B": pd.Timestamp("20130102"),
+         "C": pd.Series(1, index=list(range(4)), dtype="float32"),
+         "D": np.array([3] * 4, dtype="int32"),
+         "E": pd.Categorical(["test", "train", "test", "train"]),
+         "F": "foo",
+     }
+ )
+
+
+print ("hello world")
+print (df2)
